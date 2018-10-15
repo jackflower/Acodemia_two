d9b8b691102bac4f7477c23e9fa557b67ae4d1c2
jackflower <jackflower@poczta.onet.pl>
2018-10-15 11:55:44 +0200

    Update

---
+++ elixir/elixir.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+
+func _ready():
+	#set_physics_process(true)
+	#set_process(true)
+	pass
+	
+
+func _process(delta):
+	
+	pass
+
+
+func _on_Elixir_body_entered( body ):
+	print (body.name)
+	if(body.has_method("update_item")):
+		#print("Alfa")
+		body.update_item()
+	#queue_free()
+	pass
+	pass # replace with function body
